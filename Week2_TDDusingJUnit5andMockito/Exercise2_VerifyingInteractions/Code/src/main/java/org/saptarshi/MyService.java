438ae0054c1cc2f5948ffbeecc96749d2aea3322
LoneWolf_108 <121552564+Saptarshi-108@users.noreply.github.com>
2025-06-27 11:12:44 +0530

    Add files via upload

---
+++ Week2_TDDusingJUnit5andMockito/Exercise2_VerifyingInteractions/Code/src/main/java/org/saptarshi/MyService.java
@@ -0,0 +1,14 @@
+package org.saptarshi;
+
+
+public class MyService {
+    private final ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
